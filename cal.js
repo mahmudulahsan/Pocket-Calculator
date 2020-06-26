324199c6c72b0c023a0e04faaada8a912b6d7e02
mahmudulahsan <59706274+mahmudulahsan@users.noreply.github.com>
2020-06-27 01:38:29 +0600

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,12 @@
+function calculate(x){
+    output.display.value=output.display.value+x;
+}
+
+function help(){
+    alert("You can calculate two numbers at a time");
+}
+
+function mahi(){
+    alert("আসসালামুয়ালাইকুম, ক্যালকুলাটরটি ব্যবহার করার জন্য অসংখ্য ধন্যবাদ -মাহমুদুল আহসান মাহী :)")
+}
+
